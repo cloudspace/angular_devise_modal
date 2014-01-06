7972d52c548e7615ec2d2a1a294da77609bb5cdf
Justin Ridgewell <jridgewell@cloudspace.com>
2014-01-06 14:17:03 -0500

    Refactor Modal

--- src/modal.js
+++ src/modal.js
@@ -1,5 +1,5 @@
 deviseModal.run(function($modal, $http, Auth, $rootScope) {
-    var promise;
+    var promise = null;
 
     // A helper function to reset
     // the reference to the modal's
@@ -7,7 +7,6 @@ deviseModal.run(function($modal, $http, Auth, $rootScope) {
     function reset() {
         promise = null;
     }
-    reset();
 
     // A simplified partial function.
     // Only partials the one param passed to it.
@@ -30,26 +29,26 @@ deviseModal.run(function($modal, $http, Auth, $rootScope) {
 
         // If we have already opened the modal,
         // just register this request with its promise.
-        if (promise) { return retryRequestAfterLogin(); }
+        if (!promise) {
+            // Save a reference to the modal's promise.
+            // The promise will clean up the reference after
+            // it the modal closes, and will attempt to login
+            // the user. Chaining onto this promise will then
+            // run after the user logs in.
+            promise = $modal.open({
+                templateUrl: 'modal.html',
+                controller: function ($scope, $modalInstance) {
+                    var user = $scope.user = {};
+                    $scope.login = function () {
+                        $modalInstance.close(user);
+                    };
 
-        // Save a reference to the modal's promise.
-        // The promise will clean up the reference after
-        // it the modal closes, and will attempt to login
-        // the user. Chaining onto this promise will then
-        // run after the user logs in.
-        promise = $modal.open({
-            templateUrl: 'angularDeviseModal.html',
-            controller: function ($scope, $modalInstance) {
-                var user = $scope.user = {};
-                $scope.login = function () {
-                    $modalInstance.close(user);
-                };
-
-                $scope.dismiss = function () {
-                    $modalInstance.dismiss('cancel');
-                };
-            }
-        }).result['finally'](reset).then(Auth.login);
+                    $scope.dismiss = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                }
+            }).result['finally'](reset).then(Auth.login);
+        }
 
         retryRequestAfterLogin();
     });
