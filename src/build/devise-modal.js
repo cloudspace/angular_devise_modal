619a55c9d4ed9d8d3a3b4fe449de9c8417df7341
Justin Ridgewell <jridgewell@cloudspace.com>
2014-01-07 09:39:37 -0500

    Add in build file.

---
+++ src/build/devise-modal.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function(angular) {
+    var deviseModal = angular.module('DeviseModal', ['Devise', 'ui.bootstrap']);
+
+    // @include ../modal.js
+    // @include ../../.tmp/template.js
+})(angular);
